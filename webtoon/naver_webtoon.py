d1f97eabbc5c86f7841d8cad844ecc4d086e0237
chihwan <clghks@gmail.com>
2019-11-14 12:59:52 +0900

    크롤링 완료

--- webtoon/naver_webtoon.py
+++ webtoon/naver_webtoon.py
@@ -1,14 +1,30 @@
 import requests
 import bs4
+from .models import WebToon
 
-html = requests.get("https://comic.naver.com/webtoon/weekdayList.nhn?week=mon")
-# print(html.status_code)
-# print(html.text)
 
-bs_object = bs4.BeautifulSoup(html.text, "html.parser")
-# print(bs_object
-webtoon_list = bs_object.find('div', {'class': 'list_area daily_img'})
-# print(webtoon_list)
-webtoon_list_tags = webtoon_list.findAll('li')
-for webtoon_tag in webtoon_list_tags:
-    print(webtoon_tag.find('a')['title'])
+def naver_webtoon_day(day):
+    html = requests.get("https://comic.naver.com/webtoon/weekdayList.nhn?week=" + day)
+    # print(html.status_code)
+    # print(html.text)
+
+    bs_object = bs4.BeautifulSoup(html.text, "html.parser")
+    # print(bs_object
+    webtoon_list = bs_object.find('div', {'class': 'list_area daily_img'})
+    # print(webtoon_list)
+    webtoon_list_tags = webtoon_list.findAll('li')
+    for webtoon_tag in webtoon_list_tags:
+        webtoon = WebToon()
+        webtoon.site_name = "네이버"
+        webtoon.webtoon_name = webtoon_tag.find('a')['title']
+        webtoon.webtoon_author = webtoon_tag.find('dd', {'class': 'desc'}).text.strip()
+        webtoon.webtoon_img_url = webtoon_tag.find('a').find('img')['src']
+        webtoon.webtoon_id = "네이버_" + webtoon_tag.find('a')['title']
+
+        webtoon.save()
+
+
+def naver_webtoon():
+    week_list = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
+    for week in week_list:
+        naver_webtoon_day(week)
